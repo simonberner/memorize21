8612bccf9a57b46027ff21a5315a269b96e9d9f0
Simon Berner <sibebzh@gmail.com>
2021-08-07 14:11:26 +0200

    init: :tada: initialize the memorize code base

---
+++ Memorize21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize21
+//
+//  Created by Simon Berner on 07.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize21/Memorize21App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize21App.swift
+//  Memorize21
+//
+//  Created by Simon Berner on 07.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
