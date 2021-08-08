7cb8b35ab452a2c543983f55dc65633a5e27eba0
Simon Berner <sibebzh@gmail.com>
2021-08-08 17:02:56 +0200

    feat(cards): :construction: add some first clickable cards

--- Memorize21/ContentView.swift
+++ Memorize21/ContentView.swift
@@ -1,21 +1,85 @@
-//
-//  ContentView.swift
-//  Memorize21
-//
-//  Created by Simon Berner on 07.08.21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    // must behave like an Identifiable
+    var emojis = ["🚖","🛥","✈️","🚀"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            // \.self means: us the emoji String itself as identifier
+            ForEach(emojis, id: \.self) { emoji in
+                CardView(content: emoji)
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.blue)
     }
 }
+
+// every view in SwiftUI is immutable (can't be modified)
+// 'self' here is the whole CardView
+// the view is constantly rebuild when something changes in view
+struct CardView: View {
+    // @State turns this var in to a pointer to some boolean somewhere in the memory
+    // if the boolean in memory changes, SwiftUI will rebuild the body of this
+    // CardView
+    // (see also the official docu)
+    @State var isFaceUp: Bool = true
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        // if a function (here a view modifier) takes just one
+        // argument, we can leave out the argument name (here 'perform:')
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
