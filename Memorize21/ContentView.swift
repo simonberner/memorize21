7a09409728d5ae54c23a2e21dec22fba1c2622c8
Simon Berner <sibebzh@gmail.com>
2021-08-09 19:45:21 +0200

    feat(cards): :construction: add buttons to add and remove emoji cards
    Add buttons to add and remove emoji cards. Also introduce a LazyGridView
    with one column to hold the cards in it.

--- Memorize21/ContentView.swift
+++ Memorize21/ContentView.swift
@@ -1,18 +1,60 @@
 import SwiftUI
 
+// this is all functional programming
+// (there is no shared state, mutable data or other side effects)
+// (application state flows through pure functions)
+// (see also https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0)
 struct ContentView: View {
-    // must behave like an Identifiable
-    var emojis = ["🚖","🛥","✈️","🚀"]
+    // must behave like an Identifiable, that is why we use itself \.self as identifier
+    var emojis = ["🚖","🛥","✈️","🚀","🚗","🚊","🚟","🛶","🛫","🚜","🏎","🏍","🛵","🚐","🚤","🛴","🚲","⛵️","🚠","🚌","🦽","🛸","🏍","🚢"]
+    @State var emojiCount = 4
     
     var body: some View {
-        HStack {
-            // \.self means: us the emoji String itself as identifier
-            ForEach(emojis, id: \.self) { emoji in
-                CardView(content: emoji)
+        // VStack/HStack/ZStack are view builders
+        VStack {
+            ScrollView {
+                // Lazy about accessing the body vars in the view
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    // \.self means: use the emoji String itself as identifier
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
+            .foregroundColor(.red)
+            Spacer() // flexible spacing which takes any open space that is left
+            HStack {
+                removeButton
+                Spacer() // a spacer is always grabbing as much space as it can
+                addButton
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.blue)
+    }
+    
+    var removeButton: some View {
+        // the name of the first Button argument named 'action' (which is also a
+        // function(), can be omitted
+        // the the second argument 'label' is also a function because it is a view builder
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var addButton: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+           Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -32,7 +74,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
