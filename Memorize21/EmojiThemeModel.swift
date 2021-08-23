8a2bbd7fbd35732e266c58301e4972c1d335c12d
Simon Berner <sibebzh@gmail.com>
2021-08-23 14:17:05 +0200

    feat(cards): :construction: restrict the numberOfPairsOfCards
    If a EmojiThemeModel mistakenly specifies to have more pairs of cards
    than there are emojis available, set the numberOfPairsOfCards to be
    equal to the max number of emojis available.

--- Memorize21/EmojiThemeModel.swift
+++ Memorize21/EmojiThemeModel.swift
@@ -6,4 +6,11 @@ struct EmojiThemeModel {
     var emojis: Array<String>
     var numberOfPairsOfCards: Int
     var color: String
+    
+    init(name: String, emojis: Array<String>, numberOfPairsOfCards: Int, color: String) {
+        self.name = name
+        self.emojis = emojis
+        self.numberOfPairsOfCards = numberOfPairsOfCards > emojis.count ? emojis.count : numberOfPairsOfCards // Assignment2 - Task7
+        self.color = color
+    }
 }
