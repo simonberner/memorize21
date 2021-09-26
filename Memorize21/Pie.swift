270a6b47273bc70d00847a29ce131ce442dc0605
Simon Berner <sibebzh@gmail.com>
2021-09-26 21:29:10 +0200

    refactor(Pie): :hammer: rename var p
    Rename the var 'p' to conform to SwiftLint.

--- Memorize21/Pie.swift
+++ Memorize21/Pie.swift
@@ -16,21 +16,20 @@ struct Pie: Shape {
             y: center.y + radius * sin(CGFloat(startAngle.radians))
         )
         
-        var p = Path()
-        p.move(to: center)
-        p.addLine(to: start)
-        p.addArc(center: center,
+        var path = Path()
+        path.move(to: center)
+        path.addLine(to: start)
+        path.addArc(center: center,
                  radius: radius,
                  startAngle: startAngle,
                  endAngle: endAngle,
-                 // origin for drawing is not the cartesian coordinate system (bottom left x:0/y:0) -> https://en.wikipedia.org/wiki/Coordinate_system
-                 // we are drawing from the upper left! so all is upside down (180 degrees inverted)
-                 // from a user point of view, we kind of auto invert here the clockwise for that reason
+                    // origin for drawing is not the cartesian coordinate system (bottom left x:0/y:0) -> https://en.wikipedia.org/wiki/Coordinate_system
+                    // we are drawing from the upper left! so all is upside down (180 degrees inverted)
+                    // from a user point of view, we kind of auto invert here the clockwise for that reason
                  clockwise: !clockwise,
                  transform: .identity)
-        p.addLine(to: center)
-        return p
+        path.addLine(to: center)
+        return path
     }
 
-    
 }
