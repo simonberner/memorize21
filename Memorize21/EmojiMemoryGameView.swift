75388fe30ccb86112a59e8002e00b6b310911ffd
Simon Berner <sibebzh@gmail.com>
2021-09-27 22:15:03 +0200

    feat(Cards): :sparkles: add animation for dealing the cards
    Add an animation to deal out the cards from a deck of cards.

--- Memorize21/EmojiMemoryGameView.swift
+++ Memorize21/EmojiMemoryGameView.swift
@@ -10,34 +10,60 @@ struct EmojiMemoryGameView: View {
     // learning purposes (to better see which is the viewModel) we call it gameViewModel
     // (when an ObservedObject changes, the entire body of a view gets automatically redrawn!)
     @ObservedObject var gameViewModel: EmojiMemoryGameViewModel
-    
+    // @Namespace creates an animation namespace to allow matched geometry effects, which can be shared by other views.
+    @Namespace private var dealingNamespace
+
     // the view who draws the model
     var body: some View {
-        VStack {
-            HStack {
-                Text(gameViewModel.emojiThemeName)
-                    .font(.title)
-                Spacer()
-                Text("Score: \(gameViewModel.score)")
+        ZStack(alignment: .bottom) {
+            VStack {
+                header
+                gameBody
+                newGameButton
             }
-            .padding()
-            gameBody
-            newGameButton
+            deckBody
         }
     }
-    
+
     // keep track of dealt (laid out) cards
     // (temporary state in this view)
     @State private var dealt = Set<Int>() // things in a Set are unique
-    
+
     private func deal(_ card: EmojiMemoryGameViewModel.Card) {
         dealt.insert(card.id)
     }
-    
+
     private func isUndealt(_ card: EmojiMemoryGameViewModel.Card) -> Bool {
         !dealt.contains(card.id) // a card is undealt if it is NOT already in the Set
     }
-    
+
+    private func dealAnimation(for card: EmojiMemoryGameViewModel.Card) -> Animation {
+        var delay = 0.0
+        // firstIndex: returns the first index (Int) in the Array, where the id of a card in the cards Array
+        // is equal to the id of the passed in card (card.id)
+        if let index = gameViewModel.cards.firstIndex(where: {$0.id == card.id}) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(gameViewModel.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+
+    // the higher numbers will be in the front and the lower numbers in the back (towards the device ;))
+    // fyi: Views have a default Z index of 0, but you can provide positive or negative values that position
+    // them on top of or below other views respectively.
+    private func zIndex(of card: EmojiMemoryGameViewModel.Card) -> Double {
+        -Double(gameViewModel.cards.firstIndex(where: {$0.id == card.id}) ?? 0)
+    }
+
+    private var header: some View {
+        HStack {
+            Text(gameViewModel.emojiThemeName)
+                .font(.title2)
+            Spacer()
+            Text("Score: \(gameViewModel.score)")
+        }
+        .padding(.horizontal)
+    }
+
     private var gameBody: some View {
         AspectVGrid(items: gameViewModel.cards, aspectRatio: 2/3) { card in
             // this is a valid view builder: if else returns a view in any case
@@ -45,9 +71,14 @@ struct EmojiMemoryGameView: View {
                 Color.clear // behaves like a view here and creates a rectangle with the color 'clear'
             } else {
                 CardView(card: card, gameViewModel: gameViewModel)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
-                    // different transition for in and out with the static func asymmetric
-                    .transition(AnyTransition.asymmetric(insertion: .scale, removal: .opacity).animation(.easeInOut(duration: 2)))
+                    // different transition for in and out with: static func asymmetric
+                    // .identity: don't scale it / don't do any animation
+                    // here: use the above geometry effect when inserting a card and use the opacity transition
+                    // when a card is removed
+                    .transition(AnyTransition.asymmetric(insertion: .identity, removal: .opacity))
+                    .zIndex(zIndex(of: card))
                     .onTapGesture {
                         withAnimation {
                             gameViewModel.choose(card)
@@ -55,29 +86,45 @@ struct EmojiMemoryGameView: View {
                     }
             }
         }
+        .foregroundColor(gameViewModel.emojiThemeColor)
+        .padding(.horizontal)
+    }
+
+    var deckBody: some View {
+        ZStack {
+            // ForEach is another way to see views appearing and disappearing
+            ForEach(gameViewModel.cards.filter(isUndealt)) { card in
+                CardView(card: card, gameViewModel: gameViewModel)
+                    // links the cards to the above gameBody cards
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+                    .zIndex(zIndex(of: card))
+            }
+        }
+        .frame(width: CardConstants.undealtWidth, height: CardConstants.undealtHeight)
+        .foregroundColor(gameViewModel.emojiThemeColor)
         // put a view on screen after its container is on screen
         // when in this case the container view AspectVGrid appears
         // the func withAnimation is performed
-        .onAppear{
+        .onTapGesture {
             // deal the cards out
             // withAnimation go through all my cards
-            withAnimation {
-                for card in gameViewModel.cards {
+            for card in gameViewModel.cards {
+                withAnimation(dealAnimation(for: card)) {
                     deal(card)
                 }
             }
         }
-        .foregroundColor(gameViewModel.emojiThemeColor)
-        .padding(.horizontal)
     }
-    
+
     private var newGameButton: some View {
         Button(action: {
             // all view modifiers which can be animated are being animated inclusive those from
             // the gameBody's AspectVGrid which are needed to position the cards
             // explicit animations are mostly used for 'user intent' functions to animate change
             // in the ui
-            withAnimation{
+            withAnimation {
+                dealt = [] // resetting it to empty causes animation to happen
                 gameViewModel.startNewGame()
             }
         }, label: {
@@ -93,6 +140,15 @@ struct EmojiMemoryGameView: View {
         })
         .accessibility(label: Text("NewGame"))
     }
+
+    private struct CardConstants {
+        static let color = Color.red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDealDuration: Double = 2
+        static let undealtHeight: CGFloat = 90
+        static let undealtWidth = undealtHeight * aspectRatio
+    }
 }
 
 // every view in SwiftUI is immutable (can't be modified)
@@ -101,14 +157,15 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     let card: EmojiMemoryGameViewModel.Card
     let gameViewModel: EmojiMemoryGameViewModel
-    
+
     var body: some View {
         // GeometryReader offers space to its sub-views by telling them
         // how much space they have
-        GeometryReader (content: { geometry in
+        GeometryReader(content: { geometry in
             ZStack {
                 // 0 degrees is to the right of the iOS coordinate system (x:0/y:0 in the upper left)
-                // that is why we have to subtract 90deg (when thinking in compass rose https://en.wikipedia.org/wiki/Compass_rose)
+                // that is why we have to subtract 90deg (when thinking
+                // in compass rose https://en.wikipedia.org/wiki/Compass_rose)
                 Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
                     .padding(DrawingConstants.piePadding)
                     .opacity(DrawingConstants.pieOpacity)
@@ -123,22 +180,23 @@ struct CardView: View {
                     // so the placement of implicit animation modifiers is important
                     // this animation animates the change of the above var card.isMatched
                     .animation(.linear(duration: 1).repeatForever(autoreverses: false))
-                    .font(Font.system(size: DrawingConstants.fontSize)) // fixed font size which does not have to animate
+                    // fixed font size which does not have to animate
+                    .font(Font.system(size: DrawingConstants.fontSize))
                     .scaleEffect(scale(thatFits: geometry.size))
             }
             // the ZStack is passed as content to the Cardify ViewModifier
             .cardify(isFaceUp: card.isFaceUp, cardColor: gameViewModel.emojiThemeColorGradient)
         })
     }
-    
-    private func scale(thatFits size: CGSize) -> CGFloat{
+
+    private func scale(thatFits size: CGSize) -> CGFloat {
         min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
-    
+
     private func font(in size: CGSize) -> Font {
         Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
     }
-    
+
     // for drawing we use CGFloat, not Int or Double!
     private struct DrawingConstants {
         static let fontScale: CGFloat = 0.7
@@ -148,35 +206,6 @@ struct CardView: View {
         static let fontSize: CGFloat = 32
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
