c1650ae94990efd99a2e4260fd13d9d0f0e9baa3
Simon Berner <sibebzh@gmail.com>
2021-10-05 14:55:26 +0200

    refactor(Info): :hammer: improve the InfoView

--- Memorize21/InfoView.swift
+++ Memorize21/InfoView.swift
@@ -8,53 +8,59 @@ struct InfoView: View {
     @Binding var isShowing: Bool
 
     var body: some View {
-        ZStack {
-            if isShowing {
-                ZStack {
-                    RoundedRectangle(cornerRadius: ViewConstants.cornerRadius)
-                        .foregroundColor(Color.white)
-                    VStack {
-                        HStack {
-                            Text("Infos")
-                                .font(.headline)
-                                .padding()
-                            Button(action: {
-                                isShowing = false
-                            }, label: {
-                                Spacer()
-                                HStack {
-                                    Image(systemName: "xmark")
-                                        .foregroundColor(Color.red)
-                                }
-                                .padding()
-                            })
-                        }
-                        HStack {
-                            Text("Memorize Game")
-                            Spacer()
-                            Text("Version 1.0.0")
-                        }
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        HStack {
-                            Text("Source code")
-                            Spacer()
-                            Text(Image(systemName: "link"))
-                                .foregroundColor(.blue)
-                            Link("GitHub Repo",
-                                 destination: URL(string: "https://github.com/simonberner/memorize21")!)
-                                .foregroundColor(Color.blue)
-                        }
-                        .padding()
-                        Divider()
-                        Text("In 2021, I have started my learning journey in iOS Development. This App here reflects all the things which I have learned so far. I hope you have fun and are enjoying the game. If you face any bugs in using it on your mobile phone, please let me know by writing an email to sibebzh@gmail.com")
+        if isShowing {
+            ZStack {
+                RoundedRectangle(cornerRadius: ViewConstants.cornerRadius)
+                    .foregroundColor(Color.white)
+                VStack {
+                    HStack {
+                        Text("Infos")
+                            .font(.headline)
                             .padding()
-                        Text("Made with ❤️")
+                        Button(action: {
+                            isShowing = false
+                        }, label: {
+                            Spacer()
+                            HStack {
+                                Image(systemName: "xmark")
+                                    .foregroundColor(Color.red)
+                            }
+                            .padding()
+                        })
+                    }
+                    HStack {
+                        Text("Memorize Game")
+                        Spacer()
+                        Text("Version 1.0.0")
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    HStack {
+                        Text("Source code")
+                        Spacer()
+                        Text(Image(systemName: "link"))
+                            .foregroundColor(.blue)
+                        Link("GitHub Repo",
+                             destination: URL(string: "https://github.com/simonberner/memorize21")!)
+                            .foregroundColor(Color.blue)
+                    }
+                    .padding()
+                    Divider()
+                    HStack {
+                        Text("Game Scoring Rules")
                         Spacer()
                     }
+                    .padding()
+                    Text(ViewConstants.gameRules)
+                        .padding()
+                    Divider()
+                    Text("In 2021, I have started my learning journey in iOS Development. This App here reflects all the things which I have learned so far. I hope you have fun and are enjoying the game. If you face any bugs in using it on your mobile phone, please let me know by writing an email to sibebzh@gmail.com")
+                        .padding()
+                    Text("Made with ❤️")
+                    Spacer()
                 }
-                .foregroundColor(ViewConstants.textColor)
             }
+            .foregroundColor(ViewConstants.textColor)
         }
     }
 }
@@ -62,8 +68,12 @@ struct InfoView: View {
 private struct ViewConstants {
     static let foregroundOpacity = 0.6
     static let cornerRadius: CGFloat = 12
-    static let padding: CGFloat = 40
     static let textColor = Color.black
+    static let gameRules = """
+Rule 1: The faster you find two matching cards, the more scores you can accumulate.
+Rule 2: If you turn up a card for the second time (and further) and you don't find a match, you loose points.
+Rule 3: If you find a match before the animated pie time of 8 seconds per card has passed, you gain some extra scores.
+"""
 }
 
 // var isShowing = true
