7acece66eefdba45eab7648dedd86d47a9440638
Simon Berner <sibebzh@gmail.com>
2021-10-12 17:13:26 +0200

    add: :boom: add var to get bundle version (build version)
    Add a var to get the bundle version (build version) from the Info.plist.

--- Memorize21/MemoryGameExtensions.swift
+++ Memorize21/MemoryGameExtensions.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
+// Read values from Info.plist
+// https://sarunw.com/posts/how-to-read-info-plist/
 // https://www.hackingwithswift.com/example-code/system/how-to-read-your-apps-version-from-your-infoplist-file
 extension UIApplication {
     static var appVersion: String? {
         return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+    static var buildVersion: String? {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
     }
 }
