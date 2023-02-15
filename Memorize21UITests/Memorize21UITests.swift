f9ee003ed79b1f2e9914f424cc3a9010d7524379
Simon Berner <sibebzh@gmail.com>
2023-02-15 20:28:02 +0100

    test: :white_check_mark: add ui tests #5

--- Memorize21UITests/Memorize21UITests.swift
+++ Memorize21UITests/Memorize21UITests.swift
@@ -9,6 +9,8 @@ import XCTest
 
 class Memorize21UITests: XCTestCase {
 
+    let waitingTime = TimeInterval(integerLiteral: 1)
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -23,21 +25,52 @@ class Memorize21UITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    /// Check that the 'New Game' Button is on the game view
+    func testNewGameButtonAppears() throws {
+        // Given
+        let app = launchApp()
+
+        // When
+        app.collectionViews.buttons["selection.theme.Halloween"].tap()
+
+        // Then
+        XCTAssertTrue(app.buttons["newgame.button"].waitForExistence(timeout: waitingTime))
+    }
+
+    /// Check that the 'Info View' shows up on the screen
+    func testInfoViewAppears() throws {
+        // Given
+        let app = launchApp()
+
+        // When
+        app.collectionViews.buttons["selection.theme.Objects"].tap()
+        app.buttons["info.button"].tap()
+
+        // Then
+        XCTAssertTrue(app.staticTexts["made.smile"].waitForExistence(timeout: waitingTime))
+
+    }
+
+    private func launchApp() -> XCUIApplication {
         let app = XCUIApplication()
+        app.launchArguments = ["isRunningUITests"]
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        return app
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
+
+// Some references:
+// https://www.hackingwithswift.com/articles/148/xcode-ui-testing-cheat-sheet
+// https://www.headspin.io/blog/xctest-a-complete-guide
+// https://www.hackingwithswift.com/interview-questions/how-familiar-are-you-with-xctest-have-you-ever-created-ui-tests
+// https://tanaschita.com/20220530-getting-started-with-ui-testing-for-swiftui-with-xctest-framework/
