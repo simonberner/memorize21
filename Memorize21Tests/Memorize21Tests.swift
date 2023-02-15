14bb6225e526fda899d4c8ab22cf61e7807e1929
Simon Berner <sibebzh@gmail.com>
2023-02-15 13:04:35 +0100

    test: :white_check_mark: add utest for theme model #5

--- Memorize21Tests/Memorize21Tests.swift
+++ Memorize21Tests/Memorize21Tests.swift
@@ -8,26 +8,33 @@
 import XCTest
 @testable import Memorize21
 
+// Run Unit-Tests by not running the App: https://blog.eidinger.info/faster-unit-tests-by-not-running-the-app-startup-logic?utm_source=swiftlee&utm_medium=swiftlee_weekly&utm_campaign=issue_117
 class Memorize21Tests: XCTestCase {
 
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Check that a specific EmojiThemeModel has the correct numberOfPairsOfCards
+    func testGetEmojiThemeModel() throws {
+        // Arrange & Act
+        let emojiThemeModel = EmojiThemeViewModel().getEmojiThemeModel("Animals")
+
+        // Assert
+        XCTAssertEqual(emojiThemeModel.numberOfPairsOfCards, 5)
+
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
