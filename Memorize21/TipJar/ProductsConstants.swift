080fc1ef69909d2f2fbcbdad793c3aaafa4f43fb
Simon Berner <sibebzh@gmail.com>
2023-03-20 17:41:51 +0100

    feat: :boom: add view, model and constants for the tip jar #14

---
+++ Memorize21/TipJar/ProductsConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ProductsConstants.swift
+//  Memorize21
+//
+//  Created by Simon Berner on 20.03.23.
+//
+
+import Foundation
+
+let myTipProducts = [
+    "dev.simonberner.BuildingATipJar.tinyTip",
+    "dev.simonberner.BuildingATipJar.smallTip",
+    "dev.simonberner.BuildingATipJar.mediumTip",
+    "dev.simonberner.BuildingATipJar.largeTip"
+]
