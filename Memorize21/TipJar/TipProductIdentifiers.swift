400331691f6f1bdb30de51049b42c745f8f95f8c
Simon Berner <sibebzh@gmail.com>
2023-03-22 15:45:02 +0100

    refactor(storekit): :recycle: rename product identifiers #14

---
+++ Memorize21/TipJar/TipProductIdentifiers.swift
@@ -0,0 +1,15 @@
+//
+//  ProductsConstants.swift
+//  Memorize21
+//
+//  Created by Simon Berner on 20.03.23.
+//
+
+import Foundation
+
+let tipProductIdentifiers = [
+    "dev.simonberner.Memorize21.tinyTip",
+    "dev.simonberner.Memorize21.smallTip",
+    "dev.simonberner.Memorize21.mediumTip",
+    "dev.simonberner.Memorize21.largeTip"
+]
