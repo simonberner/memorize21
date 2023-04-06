9c945d512a9f28972131f04f0744596617323872
Simon Berner <sibebzh@gmail.com>
2023-04-06 16:36:38 +0200

    add(storekit): :sparkles: add thank you alert #14

--- Memorize21/TipJar/TipJarView.swift
+++ Memorize21/TipJar/TipJarView.swift
@@ -16,105 +16,34 @@ struct TipJarView: View {
 
     var body: some View {
 
-        VStack(spacing: 8) {
-            HStack {
-                Spacer()
-                Button {
-                    showTips.toggle()
-                } label: {
-                    Image(systemName: "xmark")
-                        .foregroundColor(Color.red)
-                }
-            }
-
+        VStack(spacing: 2) {
             Text("Enjoying the app so far?")
-                .font(.system(.title2, design: .rounded).bold())
+                .font(.system(.title3, design: .rounded).bold())
                 .multilineTextAlignment(.center)
 
             Text("You can tip me if you like!")
-                .font(.system(.body, design: .rounded))
+                .font(.system(.callout, design: .rounded))
                 .multilineTextAlignment(.center)
-                .padding(.bottom, 16)
+                .padding(.bottom, 8)
 
             ForEach(store.items) { item in
                 configureProductView(item)
             }
-
         }
         .padding(16)
         .background(Color("tip-card-background"), in: RoundedRectangle(cornerRadius: 10, style: .continuous))
-        .padding(8)
-//        .overlay(alignment: .top) {
-//            Image("tip-logo")
-//                .resizable()
-//                .frame(width: 50, height: 50)
-//                .padding(6)
-//                .background(Color.accentColor, in: RoundedRectangle(cornerRadius: 10, style: .continuous))
-//                .offset(y: -25)
-//        }
-
-        //        VStack {
-        ////            Button("Tip Me") {
-        ////                showTips.toggle()
-        ////            }
-        ////            .tint(.blue)
-        ////            .buttonStyle(.bordered)
-        //
-        //        }
-        //        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //        .overlay(alignment: .bottom) {
-        //
-        //            if showThanks {
-        //                VStack(spacing: 8) {
-        //                    Text("Thank You 💕")
-        //                        .font(.system(.title2, design: .rounded).bold())
-        //                        .multilineTextAlignment(.center)
-        //
-        //                    Text("I very much appreciate your tip! 🙏🏽")
-        //                        .font(.system(.body, design: .rounded))
-        //                        .multilineTextAlignment(.center)
-        //                        .padding(.bottom, 16)
-        //
-        //                    Button {
-        //                        showThanks.toggle()
-        //                    } label: {
-        //                        Text("Close")
-        //                            .font(.system(.title3, design: .rounded).bold())
-        //                            .tint(.white)
-        //                            .frame(height: 55)
-        //                            .frame(maxWidth: .infinity)
-        //                            .background(.blue, in: RoundedRectangle(cornerRadius: 10,
-        //                                                                    style: .continuous))
-        //                    }
-        //                }
-        //                .padding(16)
-        //                .background(Color("tip-card-background"), in: RoundedRectangle(cornerRadius: 10, style: .continuous))
-        //                .padding(.horizontal, 8)
-        //                .transition(.move(edge: .bottom).combined(with: .opacity))
-        //            }
-        //
-        //        }
-        //        .overlay {
-        //            if showTips {
-        //                Color.black.opacity(0.8)
-        //                    .ignoresSafeArea()
-        //                    .transition(.opacity)
-        //                    .onTapGesture { showTips.toggle() }
-        //                cardView
-        //                    .transition(.move(edge: .bottom).combined(with: .opacity))
-        //            }
-        //        }
-        //        .animation(.spring(), value: showTips)
-        //        .animation(.spring(), value: showThanks)
-        //        .onChange(of: store.action) { action in
-        //            if action == .successful {
-        //                showTips = false
-        //                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { showThanks.toggle() }
-        //                store.reset()
-        //            }
-        //        }
-        //        .alert(isPresented: $store.hasError, error: store.error) { }
-        //    }
+        .onChange(of: store.action, perform: { action in
+            if action == .successful {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    showThanks.toggle()
+                }
+                store.reset()
+            }
+        })
+        .alert(isPresented: $store.hasError, error: store.error) { }
+        .alert(String("Thanks for the tip! 🙏🏽"), isPresented: $showThanks) {
+            Button("Continue", role: .cancel) { showThanks.toggle() }
+        }
     }
 }
 
@@ -127,49 +56,10 @@ struct TipJarView_Previews: PreviewProvider {
 
 private extension TipJarView {
 
-//    var cardView: some View {
-//        VStack(spacing: 8) {
-//            HStack {
-//                Spacer()
-//                Button {
-//                    showTips.toggle()
-//                } label: {
-//                    Image(systemName: "xmark")
-//                        .foregroundColor(Color.red)
-//                }
-//            }
-//
-//            Text("Enjoying the app so far?")
-//                .font(.system(.title2, design: .rounded).bold())
-//                .multilineTextAlignment(.center)
-//
-//            Text("You can tip me if you like!")
-//                .font(.system(.body, design: .rounded))
-//                .multilineTextAlignment(.center)
-//                .padding(.bottom, 16)
-//
-//            ForEach(store.items) { item in
-//                configureProductView(item)
-//            }
-//
-//        }
-//        .padding(16)
-//        .background(Color("tip-card-background"), in: RoundedRectangle(cornerRadius: 10, style: .continuous))
-//        .padding(8)
-//        .overlay(alignment: .top) {
-//            Image("tip-logo")
-//                .resizable()
-//                .frame(width: 50, height: 50)
-//                .padding(6)
-//                .background(Color.accentColor, in: RoundedRectangle(cornerRadius: 10, style: .continuous))
-//                .offset(y: -25)
-//        }
-//    }
-
     private func configureProductView(_ item: Product) -> some View {
         HStack {
             VStack(alignment: .leading,
-                   spacing: 3) {
+                   spacing: 2) {
                 Text(item.displayName)
                     .font(.system(.title3, design: .rounded).bold())
                 Text(item.description)
